3ee2d7491f22de9c7dbf9e9e0401c2a79c07c283
TristaPaulk <tristapaulk1@gmail.com>
2024-11-18 21:24:51 -0500

    whackamole.py

--- whackamole.py
+++ whackamole.py
@@ -7,15 +7,19 @@ def main():
         screen = pygame.display.set_mode((640, 512))
         clock = pygame.time.Clock()
         running = True
+        mol_pos=mole_image.get_rect(topleft=(0,0))
         while running:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     running = False
                 elif event.type == pygame.MOUSEBUTTONDOWN:
-                    random.randrange(0,640,32)
+                    if mol_pos.collidepoint(event.pos):
+                        xx = random.randrange(0,640,32)
+                        yy= random.randrange(0, 512, 32)
+                        mol_pos.topleft=(xx, yy)
+
             screen.fill("light blue")
-            screen.blit(mole_image, mole_image.get_rect(topleft=(0,0)))
-            mole_image.get_rect()
+            screen.blit(mole_image, mole_image.get_rect(topleft=(mol_pos.x, mol_pos.y)))
             for x in range(0, 640, 32):
                 for y in range(0, 512, 32):
                     pygame.draw.line(screen, "dark blue", (640,y),(x,y))
